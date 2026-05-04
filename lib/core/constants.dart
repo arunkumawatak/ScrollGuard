cbd36d8f79ae2d6de8e2691921d80d2c67754160
arunkumawatak <arunkumawat8989@gmail.com>
2026-05-04 23:55:26 +0530

    fix:did changes in constant file

---
+++ lib/core/constants.dart
@@ -0,0 +1,9 @@
+class Constants {
+  static const String methodChannel = "scroll_guard";
+  static const String hiveUserBox = 'user';
+  static const String hiveLimitsBox = 'limits';
+  static const String hiveUsageBox = 'usage';
+  static const List<int> predefinedLimits = [1,30, 60, 120];
+  static const String notificationMode = "notification";
+  static const String blockMode = "block";
+}
